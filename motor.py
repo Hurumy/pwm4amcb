c8d3e4372b46b9ac5bd7fa947805fe13e0375e40
Hurumy <sarrendish@gmail.com>
2024-01-18 13:21:21 +0900

    インデントをタブに統一

--- motor.py
+++ motor.py
@@ -5,82 +5,82 @@ import math
 
 class ControllMotor:
 	def __init__(self):
-        self.pi = math.pi
-        rospy.loginfo('ControllMotor start.')
-        self.rev = False
-        self.motor_rpm = 0.063 # wheel size [m]
-        self.gearratio = 8.27
-        self.neutral_duty = 7.500
-        self.freq = 50
-        self.motor_rpm = Int64()
-        self.duty = float64()
-        self.max_rpm = 12000
-        self.min_rpm = 0
-        # maxduty != mindutyじゃないとゼロ除算になります
-        self.max_duty = self.neutral_duty + 0.8
-        self.min_duty = self.neutral_duty - 0.8
-        self.pinnum = 12 # PWM信号を書き出すピンの番号(BOARD指定)
-        GPIO.setmode(GPIO.BOARD)		# ピンの指定方法を選ぶ
-        GPIO.setup(self.pinnum, GPIO.OUT)
-        self.pwm = GPIO.PWM(self.pinnum, self.freq) # PWMのインスタンスを作る
-        rospy.loginfo('ESC Calibration start.')
-        self.pwm.start(self.neutral_duty)
-        sleep(5)
-        rospy.loginfo('ESCから、1回の長いビープ音がしたことを確認してください。していなければ、再実行してください。')
-        rospy.loginfo('ESC Calibration end.')
-        rospy.loginfo('Initialising ControllMotor Completed.')
+		self.pi = math.pi
+		rospy.loginfo('ControllMotor start.')
+		self.rev = False
+		self.motor_rpm = 0.063 # wheel size [m]
+		self.gearratio = 8.27
+		self.neutral_duty = 7.500
+		self.freq = 50
+		self.motor_rpm = Int64()
+		self.duty = float64()
+		self.max_rpm = 12000
+		self.min_rpm = 0
+		# maxduty != mindutyじゃないとゼロ除算になります
+		self.max_duty = self.neutral_duty + 0.8
+		self.min_duty = self.neutral_duty - 0.8
+		self.pinnum = 12 # PWM信号を書き出すピンの番号(BOARD指定)
+		GPIO.setmode(GPIO.BOARD)		# ピンの指定方法を選ぶ
+		GPIO.setup(self.pinnum, GPIO.OUT)
+		self.pwm = GPIO.PWM(self.pinnum, self.freq) # PWMのインスタンスを作る
+		rospy.loginfo('ESC Calibration start.')
+		self.pwm.start(self.neutral_duty)
+		sleep(5)
+		rospy.loginfo('ESCから、1回の長いビープ音がしたことを確認してください。していなければ、再実行してください。')
+		rospy.loginfo('ESC Calibration end.')
+		rospy.loginfo('Initialising ControllMotor Completed.')
 
-    def output(self):
-        self.pwm.ChangeDutyCycle(self.duty)
+	def output(self):
+		self.pwm.ChangeDutyCycle(self.duty)
 
-    def vel2rpm(self, linear_vel_x): # convert speed to rpm
-        # [m/s]
-        # スピードが負ならバックモードをTrueにする
-        if linear_vel_x < 0.0:
-            self.rev = True
-            linear_vel_x = linear_vel_x * -1
-        else:
-            self.rev = False
-        wheel_speed = linear_vel_x / (self.wheel_size * 2.0 * self.pi) # wheel_speed [1/s]
-        # スピードが0ならブレーキをかける
-        if linear_vel_x == 0.0:
-            self.motor_rpm = 0
-        else:
-            self.motor_rpm = wheel_speed * self.gearratio * 60.0
+	def vel2rpm(self, linear_vel_x): # convert speed to rpm
+		# [m/s]
+		# スピードが負ならバックモードをTrueにする
+		if linear_vel_x < 0.0:
+			self.rev = True
+			linear_vel_x = linear_vel_x * -1
+		else:
+			self.rev = False
+		wheel_speed = linear_vel_x / (self.wheel_size * 2.0 * self.pi) # wheel_speed [1/s]
+		# スピードが0ならブレーキをかける
+		if linear_vel_x == 0.0:
+			self.motor_rpm = 0
+		else:
+			self.motor_rpm = wheel_speed * self.gearratio * 60.0
 
-    def rpm2duty(self): # convert rpm to duty
-        rpm_rate = (self.max_rpm-self.min_rpm)/(self.max_duty-self.neutral_duty) # 1%に対するrpm
-        duty_diff = self.motor_rpm / rpm_rate
-        if self.rev == False and self.motor_rpm > 0:
-            self.duty = self.neutral_duty + duty_diff
-        elif self.rev == True and self.motor_rpm > 0
-            self.duty = self.neutral_duty - duty_diff
-        elif self.motor_rpm == 0:
-            self.duty = self.neutral_duty
+	def rpm2duty(self): # convert rpm to duty
+		rpm_rate = (self.max_rpm-self.min_rpm)/(self.max_duty-self.neutral_duty) # 1%に対するrpm
+		duty_diff = self.motor_rpm / rpm_rate
+		if self.rev == False and self.motor_rpm > 0:
+			self.duty = self.neutral_duty + duty_diff
+		elif self.rev == True and self.motor_rpm > 0
+			self.duty = self.neutral_duty - duty_diff
+		elif self.motor_rpm == 0:
+			self.duty = self.neutral_duty
 
-        #速度制約
-        if self.duty > self.max_duty:
-            rospy.loginfo('Speed is too high: limited')
-            self.duty = self.max_duty
-        elif self.duty < self.min_duty:
-            rospy.loginfo('Rev Speed is too high: limited')
-            self.duty = self.min_duty
+		#速度制約
+		if self.duty > self.max_duty:
+			rospy.loginfo('Speed is too high: limited')
+			self.duty = self.max_duty
+		elif self.duty < self.min_duty:
+			rospy.loginfo('Rev Speed is too high: limited')
+			self.duty = self.min_duty
 
-    def duty2PWM(self):
-        if self.motor_rpm == 0:
-            self.pwm.ChangeDutyCycle(self.neutral_duty)
-        else:
-            output()
+	def duty2PWM(self):
+		if self.motor_rpm == 0:
+			self.pwm.ChangeDutyCycle(self.neutral_duty)
+		else:
+			output()
 
-    def controll_motor_loop(self, vel_x):
-        vel2rpm(vel_x)
-        rpm2duty()
-        duty2PWM()
+	def controll_motor_loop(self, vel_x):
+		vel2rpm(vel_x)
+		rpm2duty()
+		duty2PWM()
 
-    def motor_stop(self):
-        GPIO.cleanup()
-        rospy.loginfo('ControllMotor is stopped.')
-        self.pwm.stop() #終了
+	def motor_stop(self):
+		GPIO.cleanup()
+		rospy.loginfo('ControllMotor is stopped.')
+		self.pwm.stop() #終了
 
 
 
--- servo.py
+++ servo.py
@@ -5,70 +5,70 @@ import math
 
 class ControllHandle:
 	def __init__(self):
-        self.pi = math.pi
-        rospy.loginfo('ControllHandle start.')
-        self.neutral_duty = 7.500 # [1]
-        self.neutral_angle = 90.0 # 度数法
-        self.wheel_base = 0.257 # [m]
-        self.servocoef = 1.0
-        self.freq = 50 # [Hz]
-        self.wheelang = 0 # [rad]
-        self.servrot = 0 # [deg]
-        self.pinnum = 40 # PWM信号を書き出すピンの番号(BOARD指定)
-        self.serv_maxrot = self.neutral_angle + 30.0 # 度数法
-        self.serv_minrot = self.neutral_angle - 30.0 # 度数法
-        self.max_duty = 10.0
-        self.min_duty = 5.0
-        self.duty = float64()
-        GPIO.setmode(GPIO.BOARD)		# ピンの指定方法を選ぶ
-        GPIO.setup(self.pinnum, GPIO.OUT)
-        self.pwm = GPIO.PWM(self.pinnum, self.freq) # PWMのインスタンスを作る
-        self.pwm.start(self.neutral_duty)
-        rospy.loginfo('サーボモータの傾きを初期化しました。この状態でタイヤをまっすぐにしてください。5秒スリープします。')
-        sleep(5)
-        rospy.loginfo('Initialising ControllHandle is Completed.')
+		self.pi = math.pi
+		rospy.loginfo('ControllHandle start.')
+		self.neutral_duty = 7.500 # [1]
+		self.neutral_angle = 90.0 # 度数法
+		self.wheel_base = 0.257 # [m]
+		self.servocoef = 1.0
+		self.freq = 50 # [Hz]
+		self.wheelang = 0 # [rad]
+		self.servrot = 0 # [deg]
+		self.pinnum = 40 # PWM信号を書き出すピンの番号(BOARD指定)
+		self.serv_maxrot = self.neutral_angle + 30.0 # 度数法
+		self.serv_minrot = self.neutral_angle - 30.0 # 度数法
+		self.max_duty = 10.0
+		self.min_duty = 5.0
+		self.duty = float64()
+		GPIO.setmode(GPIO.BOARD)		# ピンの指定方法を選ぶ
+		GPIO.setup(self.pinnum, GPIO.OUT)
+		self.pwm = GPIO.PWM(self.pinnum, self.freq) # PWMのインスタンスを作る
+		self.pwm.start(self.neutral_duty)
+		rospy.loginfo('サーボモータの傾きを初期化しました。この状態でタイヤをまっすぐにしてください。5秒スリープします。')
+		sleep(5)
+		rospy.loginfo('Initialising ControllHandle is Completed.')
 
-    def output(self):
-        self.pwm.ChangeDutyCycle(self.duty)
+	def output(self):
+		self.pwm.ChangeDutyCycle(self.duty)
 
-    def omega2rot(self, linear_vel_x, omega_z): # convert omega to rotate angle
-        # [m/s], [rad/s]
-        # タイヤ角度の設定
-        if linear_vel_x == 0.0:
-            self.wheelang = self.wheelang
-        else:
-            self.wheelang = math.asin(omega_z * self.wheel_base / linear_vel_x) #[rad]
+	def omega2rot(self, linear_vel_x, omega_z): # convert omega to rotate angle
+		# [m/s], [rad/s]
+		# タイヤ角度の設定
+		if linear_vel_x == 0.0:
+			self.wheelang = self.wheelang
+		else:
+			self.wheelang = math.asin(omega_z * self.wheel_base / linear_vel_x) #[rad]
 
-    def wheelrot2servrot(self):
-        # タイヤの曲がり角とサーボモーターの曲がり角を揃えるための係数などをここでかける
-        ang_d = math.degrees(self.wheelang) # 弧度法表記から度数法表記に変換
-        ang_d = ang_d + self.neutral_angle # ang_dは正負の数なので、中心を設定
-        self.servrot = ang_d * self.servocoef # サーボモータに設定すべき角度(度数法)
-        if self.servrot > self.serv_maxrot:
-            rospy.loginfo('Angle is too big: limited')
-            self.servrot = self.serv_maxrot
-        elif self.servrot < self.serv_minrot:
-            rospy.loginfo('Angle is too small: limited')
-            self.servrot = self.serv_minrot
+	def wheelrot2servrot(self):
+		# タイヤの曲がり角とサーボモーターの曲がり角を揃えるための係数などをここでかける
+		ang_d = math.degrees(self.wheelang) # 弧度法表記から度数法表記に変換
+		ang_d = ang_d + self.neutral_angle # ang_dは正負の数なので、中心を設定
+		self.servrot = ang_d * self.servocoef # サーボモータに設定すべき角度(度数法)
+		if self.servrot > self.serv_maxrot:
+			rospy.loginfo('Angle is too big: limited')
+			self.servrot = self.serv_maxrot
+		elif self.servrot < self.serv_minrot:
+			rospy.loginfo('Angle is too small: limited')
+			self.servrot = self.serv_minrot
 
-    def rot2PWM(self):
-        duty_unit = (self.max_duty - self.min_duty)/(self.serv_maxrot - self.serv_minrot)
-        # サーボの回転1°あたりのdutyの変化量
-        self.duty = self.servrot * duty_unit
-        if self.duty > self.max_duty:
-            rospy.loginfo('Duty is too big: limited')
-            self.duty = self.max_duty
-        elif self.duty < self.min_duty:
-            rospy.loginfo('Duty is too small: limited')
-            self.duty = self.min_duty
-        output()
+	def rot2PWM(self):
+		duty_unit = (self.max_duty - self.min_duty)/(self.serv_maxrot - self.serv_minrot)
+		# サーボの回転1°あたりのdutyの変化量
+		self.duty = self.servrot * duty_unit
+		if self.duty > self.max_duty:
+			rospy.loginfo('Duty is too big: limited')
+			self.duty = self.max_duty
+		elif self.duty < self.min_duty:
+			rospy.loginfo('Duty is too small: limited')
+			self.duty = self.min_duty
+		output()
 
-    def controll_handle_loop(self, linear_vel_x, angular_vel_z): # omega[rad/s]
-        omega2rot(linear_vel_x, angular_vel_z)
-        wheelrot2servrot()
-        rot2PWM()
+	def controll_handle_loop(self, linear_vel_x, angular_vel_z): # omega[rad/s]
+		omega2rot(linear_vel_x, angular_vel_z)
+		wheelrot2servrot()
+		rot2PWM()
 
-    def handle_stop(self):
-        GPIO.cleanup()
-        rospy.loginfo('ControllHandle is stopped.')
-        self.pwm.stop() #終了
+	def handle_stop(self):
+		GPIO.cleanup()
+		rospy.loginfo('ControllHandle is stopped.')
+		self.pwm.stop() #終了
